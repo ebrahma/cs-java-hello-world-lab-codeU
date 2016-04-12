ed50142aa00e4101aa8c4e7d34c07ca9b9b8925b
ebrahma <ebrahma@gmail.com>
2016-04-12 16:39:02 -0400

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,9 +3,13 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+    	String version = System.getProperty("java.version");
+    	String[] parts = version.split("\\_")[0].split("\\.");
+    	return Double.parseDouble(parts[0] + "." + parts[1]);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+    	System.out.println(getVersion());
+    	getVersion();
     }
 }
